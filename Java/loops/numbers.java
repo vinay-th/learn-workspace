583ac56c9f41a21b6f57e1a311af7c5db40781be
Vinay Thakor <144613261+vinay-th@users.noreply.github.com>
2024-04-17 18:34:19 +0530

    Add files via upload

---
+++ Java/loops/numbers.java
@@ -0,0 +1,9 @@
+public class numbers {
+    public static void main(String[] argc) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
