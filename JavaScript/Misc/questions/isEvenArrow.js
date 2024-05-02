52ee1f310fc83681b5065943d99b0f1679feb54d
Vinay Thakor <144613261+vinay-th@users.noreply.github.com>
2024-05-02 09:42:37 +0530

    Add files via upload

---
+++ JavaScript/Misc/questions/isEvenArrow.js
@@ -0,0 +1,6 @@
+let a = 7;
+let isEven = (n) => {
+  if (n % 2 == 0) return true;
+  else return false;
+};
+console.log("The num " + a + " is even :" + isEven(a));
---
+++ JavaScript/Misc/questions/squareArrow.js
@@ -0,0 +1,3 @@
+let a = 5;
+let sqr = (n) => n * n;
+console.log(sqr(a));
