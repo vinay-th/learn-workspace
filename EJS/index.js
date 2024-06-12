034501ec3c195bba6666e7b1357d7c85e2e6e5c2
Vinay Thakor <vinay.th.24@gmail.com>
2024-06-12 11:02:56 +0530

    Add files via upload

--- EJS/index.js
+++ EJS/index.js
@@ -1,8 +1,12 @@
 const { log } = require("console");
+const exp = require("constants");
 const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+
+app.use(express.static("public/css"));
+app.use(express.static("public/js"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -11,7 +15,8 @@ app.listen(port, () => {
   console.log("Listening on port: ", port);
 });
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.ren;
+  der("home.ejs");
 });
 
 app.get("/rolldice", (req, res) => {
@@ -20,7 +25,14 @@ app.get("/rolldice", (req, res) => {
   res.render("rolldice.ejs", { dice });
 });
 
+// app.get("/ig/:username", (req, res) => {
+//   let { username } = req.params;
+//   const followers = ["john", "jane", "mike", "sarah"];
+//   res.render("instagram.ejs", { username, followers });
+// });
+
 app.get("/ig/:username", (req, res) => {
+  const instaData = require("./data.json");
   let { username } = req.params;
-  res.render("instagram.ejs", { username });
+  res.render("instagram.ejs", { data: instaData[username] });
 });
